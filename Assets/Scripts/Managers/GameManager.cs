b0b9fd9f1977ff146b463f826e96c5a394c1b17a
scoow <85620078+scoow@users.noreply.github.com>
2023-04-18 00:06:45 +0300

    Добавил пустые скрипты
    GameManager
    LevelGenerator

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace TDShooter.Managers.GameManager
+{
+    public class GameManager : MonoBehaviour
+    {
+
+    }
+}
---
+++ Assets/Scripts/Managers/LevelGenerator.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+namespace TDShooter.Managers
+{
+    public class LevelGenerator : MonoBehaviour
+    {
+    }
+}
