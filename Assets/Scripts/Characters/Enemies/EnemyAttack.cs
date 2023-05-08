6956dd1b3c7c58bd3b47809ef7ca80a93a667bd9
scoow <85620078+scoow@users.noreply.github.com>
2023-05-09 02:27:03 +0300

    Добавил врагам возможность атаковать
    скрипт EnemyAttack
    
    В нём задаётся цель атаки. По-умолчанию, это игрок
    Дальность атаки
    КД между атаками

---
+++ Assets/Scripts/Characters/Enemies/EnemyAttack.cs
@@ -0,0 +1,67 @@
+using TDShooter.Input;
+using UnityEngine;
+
+namespace TDShooter.Enemies
+{
+    public class EnemyAttack : MonoBehaviour
+    {
+        /// <summary>
+        /// Цель атаки
+        /// </summary>
+        private Transform _target;
+
+        /// <summary>
+        /// Дальность атаки
+        /// </summary>
+        [SerializeField]
+        private float _attackRange = 2;
+        [SerializeField]
+        private float _attackCooldown = 4;//кд до атаки
+        private float _timer;//время для отсчёта кд
+        private void Awake()
+        {
+            _target = FindObjectOfType<PlayerControl>().transform;
+        }
+        private void OnEnable()
+        {
+            _timer = _attackCooldown;
+        }
+        /// <summary>
+        /// Проверка, достаём ли до цели
+        /// </summary>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        private bool TargetInAttackRange(Transform target)
+        {
+            return Distance(target.transform) < _attackRange;
+        }
+        private float Distance(Transform target)
+        {
+            return Vector3.Distance(transform.position, target.position);
+        }
+        private void Update()
+        {
+            CalculateAttackCoolDownTime();
+        }
+
+        private void CalculateAttackCoolDownTime()
+        {
+            _timer -= Time.deltaTime;
+            if (_timer <= 0)
+            {
+                _timer = _attackCooldown;
+
+                if (TargetInAttackRange(_target))
+                {
+                    Attack();
+                }
+            }
+        }
+
+        private void Attack()
+        {
+            Debug.Log("Атакую!");
+
+        }
+    }
+}
