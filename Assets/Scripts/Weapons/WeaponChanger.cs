a0625f59b96ebfb2bd4eff288f42dcfbfa4f715b
scoow <85620078+scoow@users.noreply.github.com>
2023-05-16 21:46:21 +0300

    Добавил переключение модели оружия в руках

--- Assets/Scripts/Weapons/WeaponChanger.cs
+++ Assets/Scripts/Weapons/WeaponChanger.cs
@@ -9,15 +9,23 @@ namespace TDShooter.UI
         [SerializeField] Image _currentWeaponImage;
         [SerializeField] Sprite _machineGunSprite;
         [SerializeField] Sprite _plasmaGunSprite;
+
+        //[SerializeField] GameObject _currentWeaponModel;
+        [SerializeField] GameObject _machineGun;
+        [SerializeField] GameObject _plasmaGun;
         public void ChangeWeapon(WeaponType type)
         {
             switch (type)
             {
                 case WeaponType.Machinegun:
                     _currentWeaponImage.sprite = _machineGunSprite;
+                    _plasmaGun.SetActive(false);
+                    _machineGun.SetActive(true);
                     break;
                 case WeaponType.Plasmagun:
                     _currentWeaponImage.sprite = _plasmaGunSprite;
+                    _machineGun.SetActive(false);
+                    _plasmaGun.SetActive(true);
                     break;
             }
         }
