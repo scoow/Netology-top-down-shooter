adc4f8402022744c374f4978fa0e17573e258a21
scoow <85620078+scoow@users.noreply.github.com>
2023-05-28 20:40:45 +0300

    Добавил портал
    Который будет переносить на арену для битвы с боссом

---
+++ Assets/Portal.cs
@@ -0,0 +1,27 @@
+using Cysharp.Threading.Tasks;
+using System;
+using System.Threading.Tasks;
+using TDShooter.Input;
+using UnityEngine;
+
+namespace TDShooter.Level
+{
+    public class Portal : MonoBehaviour
+    {
+        [SerializeField] private Vector3 _destinationPoint;
+        [SerializeField] private float _delayBeforeTeleportation;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!other.TryGetComponent<PlayerControl>(out var teleportTarget)) return;
+            
+            _ = TeleportAsync(teleportTarget);
+        }
+
+        private async Task TeleportAsync(PlayerControl teleportTarget)
+        {
+            await UniTask.Delay(TimeSpan.FromSeconds(_delayBeforeTeleportation));
+            teleportTarget.transform.position = _destinationPoint;
+        }
+    }
+}
